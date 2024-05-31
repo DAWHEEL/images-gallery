6f959cc67045c9d0460e74e60e3e4930b05dd720
Dave Wheeler <dawheel67@gmail.com>
2024-05-31 07:59:35 -0700

    eslint and prettier setup

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,30 +1,33 @@
-import React from "react";
-import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import React from 'react';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 
 const Search = ({ word, setWord, handleSubmit }) => {
-    return (
-      <Container className="mt-4">
-        <Row className="justify-content-center">
-          <Col xs={12} md={8}>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group as={Row}>
-                <Col xs={9}>
-                  <Form.Control
-                    type="text"
-                    value={word}
-                    onChange={ (e) => setWord(e.target.value) }
-                    placeholder="Search for new image..." />
-                </Col>
-   
-                <Col>
-                  <Button variant="primary" type="submit">Search</Button>
-                </Col>
-              </Form.Group>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  };
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Row}>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image..."
+                />
+              </Col>
 
-export default Search
+              <Col>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Search;
